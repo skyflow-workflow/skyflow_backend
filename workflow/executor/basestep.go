2efe071294897ae40e1984e68d66777238b851ce
mmtbak <47457296+mmtbak@users.noreply.github.com>
2025-09-11 01:34:08 +0800

    add

--- workflow/executor/basestep.go
+++ workflow/executor/basestep.go
@@ -1,11 +1,15 @@
 package executor
 
 import (
+	"encoding/json"
 	"fmt"
+	"time"
 
+	"github.com/mmtbak/microlibrary/rdb"
 	"github.com/skyflow-workflow/skyflow_backbend/pkg/toolkit"
 	"github.com/skyflow-workflow/skyflow_backbend/workflow/parser/states"
 	"github.com/skyflow-workflow/skyflow_backbend/workflow/repository/queue"
+	"trpc.group/trpc-go/tnet/log"
 
 	"github.com/skyflow-workflow/skyflow_backbend/workflow/po"
 )
@@ -37,17 +41,17 @@ func NewExecutionStep(dbStep *po.Step, executor *Executor) (*ExecutionStep, erro
 }
 
 // GetBone  Get Execution Step Status and return StepBone  struct
-func (s *ExecutionStep) GetBone() StepBone {
+func (step *ExecutionStep) GetBone() StepBone {
 
 	bone := StepBone{
 		BaseBone: BaseBone{
-			BaseBone: s.State.GetBone().BaseBone,
-			Status:   s.Data.Status,
-			StepID:   s.Data.ID,
+			BaseBone: step.State.GetBone().BaseBone,
+			Status:   step.Data.Status,
+			StepID:   step.Data.ID,
 		},
 	}
-	if s.Data.ExecuteIndex > 0 {
-		bone.Index = s.Data.ExecuteIndex
+	if step.Data.ExecuteIndex > 0 {
+		bone.Index = step.Data.ExecuteIndex
 	}
 	return bone
 }
@@ -55,50 +59,221 @@ func (s *ExecutionStep) GetBone() StepBone {
 // ProcessEvent process step event
 // ProcessEvent is a placeholder method for processing events.
 // It currently returns an error indicating that the method is not implemented.
-func (s *ExecutionStep) ProcessEvent(message queue.InnerMessage) error {
+func (step *ExecutionStep) ProcessEvent(message queue.InnerMessage) error {
 	return fmt.Errorf("method not implement")
 }
 
 // DecodeInput decode intput to json
-func (s *ExecutionStep) DecodeInput() (any, error) {
-	i, err := toolkit.DecodeStringToMap(s.Data.Input)
+func (step *ExecutionStep) DecodeInput() (any, error) {
+	i, err := toolkit.DecodeStringToMap(step.Data.Input)
 	return i, err
 }
 
 // DecodeOutput decode output to json
-func (s *ExecutionStep) DecodeOutput() (any, error) {
-	i, err := toolkit.DecodeStringToMap(s.Data.Output)
+func (step *ExecutionStep) DecodeOutput() (any, error) {
+	i, err := toolkit.DecodeStringToMap(step.Data.Output)
 	return i, err
 }
 
 // GetInput 获得经过计算的input
-func (s *ExecutionStep) GetInput() (any, error) {
-	i, err := s.DecodeInput()
+func (step *ExecutionStep) GetInput() (any, error) {
+	i, err := step.DecodeInput()
 	if err != nil {
 		return nil, err
 	}
-	ii, err := s.State.GetBaseState().GetParametersInput(i)
+	ii, err := step.State.GetBaseState().GetParametersInput(i)
 	return ii, err
 }
 
 // GetNextStep  return (NextState string , output any, err error )
-func (s *ExecutionStep) GetNextStep(output any) (NextStep, error) {
+func (step *ExecutionStep) GetNextStep(output any) (NextStep, error) {
 
 	var err error
 	var sns NextStep
 	var input any
-	input, err = s.DecodeInput()
+	input, err = step.DecodeInput()
 	if err != nil {
 		return sns, nil
 	}
-	ns, err := s.State.GetBaseState().GetNextState(input, output)
+	ns, err := step.State.GetBaseState().GetNextState(input, output)
 	if err != nil {
 		return sns, err
 	}
 	nextstep := NextStep{
 		NextState: ns,
-		GroupID:   s.Data.GroupID,
+		GroupID:   step.Data.GroupID,
 	}
 	return nextstep, nil
 
+}
+
+// Init Init初始化state
+// NOCC:golint/fnsize("设计如此")
+func (step *ExecutionStep) Init(msg queue.InnerMessage) error {
+
+	var err error
+	starttime := time.Now()
+
+	var dbStep po.Step
+	var basedbstep po.Step
+	var dbExecution po.Execution
+	var exelock lock.Lock
+
+	var stateexemsg = StateExecuteMessage{
+		Unblock: false,
+	}
+	// 兼容历史消息
+	if msg.Data != "" {
+		err = json.Unmarshal([]byte(msg.Data), &stateexemsg)
+		if err != nil {
+			return err
+		}
+	}
+
+	// 提前取出 Input字段， 避免在事务中查询
+	basedbstep, err = step.ExecutionService.QueryStepByID(step.Data.ID, append(StepFields.L1, "input"), nil)
+
+	if err != nil {
+		log.Error(err.Error())
+		return err
+	}
+
+	tx, maker := step.ExecutionService.metadb.NewSessionMaker(nil)
+	defer maker.Close(&err)
+
+	// 如果打开了 ExecuteIndex 开关，需要计算每个步骤的ExecuteIndex
+	if step.ExecutionService.EnableExecuteIndex {
+		// 要计算执行的Index , 需要加全局锁
+		txf := rdb.ForUpdate(tx)
+		dbExecution, err = step.ExecutionService.QueryExecutionByID(step.Data.ExecutionID, []string{"id", "max_execute_index"}, txf)
+		if err != nil {
+			return err
+		}
+	} else {
+		// 减少一次额外的查询
+		dbExecution, err = step.ExecutionService.QueryExecutionByID(dbStep.ExecutionID, []string{"id", "max_execute_index"}, tx)
+		if err != nil {
+			return err
+		}
+	}
+
+	// 从数据库重新读取数据， 避免并发问题， 减少读取的字段数
+	fields := []string{"id", "type", "execute_count", "execute_index", "execution_id"}
+	dbStep, err = step.ExecutionService.QueryStepByID(step.Data.ID, fields, tx)
+
+	if err != nil {
+		log.Error(err.Error())
+		return err
+	}
+
+	// 判断 是否超过 MaxExecuteTimes, 如果超过， 则拒绝执行
+	if dbStep.ExecuteCount >= int(step.BaseSate.MaxExecuteTimes) {
+		err = fmt.Errorf("state execute count reach 'MaxExecuteTimes' argument")
+		return err
+	}
+	// 开始准备初始化
+	// 当前V1阶段
+	// 不论何种情况，都先查一下
+	var maxindex, executionindex, executecount int
+
+	maxindex = dbExecution.MaxExecuteIndex
+	// maxindex 代表当前最大的执行Index。
+	// 如果当前是未执行节点，  则dbstep.ExecuteIndex = maxindex +1; maxindex = dbstep.ExecuteIndex
+	// 如果当前是已经执行的节点，则 都保持不变 。
+	if step.ExecutionService.EnableExecuteIndex {
+
+		//初始化 基础结构数据, 计算当前的ExecuteIndex
+		// maxindex 是
+		if dbStep.ExecuteIndex < 0 {
+			if maxindex <= 0 {
+				executionindex = 1
+			} else {
+				executionindex = maxindex + 1
+			}
+		} else {
+			// 如果当前的已经 > 0
+			// 则保持不变
+			executionindex = dbStep.ExecuteIndex
+		}
+	} else {
+		if dbStep.ExecuteIndex < 0 {
+			executionindex = 0 - dbStep.ExecuteIndex
+		} else {
+			executionindex = dbStep.ExecuteIndex
+		}
+	}
+	// 非Task 状态需要在处理流程中计数器 +1 ，
+	// 因为task节点可以自动重试，可以在重试逻辑中自动累加计数器，所以需要在执行逻辑中 +1
+	// 但是其他类型节点不需要，可以在全局初始化的时候 +1， 这些节点重试的时候，进入初始化流程中， 在这里再次 +1
+	if dbStep.Type == grammar.StateType.Task {
+		executecount = dbStep.ExecuteCount
+	} else {
+		executecount = dbStep.ExecuteCount + 1
+	}
+
+	// update state information
+	now := time.Now()
+	updatestate := po.Step{
+		Status:       string(StepStatus.Initialize),
+		ExecuteIndex: executionindex,
+		ExecuteCount: executecount,
+		StartTime:    &now,
+		Data:         "",
+	}
+
+	err = tx.Where(po.Step{ID: dbStep.ID}).Updates(&updatestate).Error
+	if err != nil {
+		return err
+	}
+
+	//如果打开了 ExecuteIndex 开关
+	// 那么更新Execution的max_execute_index 值 ,不然就放弃，保持 0
+	// 如果当前的index > maxindex , 更新maxindex
+	if maxindex < executionindex {
+		updateexe := po.Execution{
+			MaxExecuteIndex: executionindex,
+		}
+		err = tx.Where(po.Execution{ID: dbStep.ExecutionID}).Updates(&updateexe).Error
+		if err != nil {
+			return err
+		}
+	}
+	tx.Commit()
+
+	// 尽量提前解锁
+	if exelock != nil {
+		exelock.Unlock()
+	}
+
+	event0 := ExecutionEvent{
+		ExecutionID: dbStep.ExecutionID,
+		StepID:      dbStep.ID,
+		StepName:    basedbstep.Name,
+		StartTime:   starttime,
+		FinishTime:  now,
+		Data: EventContent_StateEntered{
+			Input: basedbstep.Input,
+		},
+	}
+	event1 := ExecutionEvent{
+		ExecutionID: dbStep.ExecutionID,
+		StepID:      dbStep.ID,
+		StepName:    basedbstep.Name,
+		StartTime:   starttime,
+		FinishTime:  now,
+		Data: EventContent_StateInit{
+			Action:         "StartState",
+			ExecutionIndex: executionindex,
+			ExecutionCount: updatestate.ExecuteCount,
+		},
+	}
+	step.ExecutionService.SendExecutionEvents(event0, event1)
+
+	// message queue send create message
+	message := NewStateMessage(dbStep.ExecutionID, MessageType.StateExecute, dbStep.ID, stateexemsg)
+	err = step.ExecutionService.innerqueue.SendInnerMessage(message, time.Now())
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- workflow/executor/step.go
+++ workflow/executor/step.go
@@ -1,6 +1,10 @@
 package executor
 
 import (
+	"fmt"
+
+	"github.com/skyflow-workflow/skyflow_backbend/workflow/parser/states"
+	"github.com/skyflow-workflow/skyflow_backbend/workflow/po"
 	"github.com/skyflow-workflow/skyflow_backbend/workflow/repository/queue"
 )
 
@@ -17,41 +21,40 @@ type Step interface {
 	ProcessEvent(queue.InnerMessage) error
 }
 
-// // NewStepFromData ...
-// func NewStepFromData(dbStep *po.Step, executor *Executor) (Step, error) {
-// 	var step Step
-// 	var err error
+// NewStepFromData ...
+func NewStepFromData(dbStep *po.Step, executor *Executor) (Step, error) {
+	var step Step
+	var err error
 
-// 	wfstep, err := parser.ParserStep(dbStep.Definition, workflowtype)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	node := wfstep.GetNode()
+	state, err := executor.Parser.ParseState(dbStep.Definition)
+	if err != nil {
+		return nil, err
+	}
+	node := state.GetBone()
+	switch dbStep.Type {
+	case string(states.StateTypes.Task):
+		step, err = NewTaskFromData(dbStep, executor)
+	// case grammar.StateType.Choice:
+	// 	step, err = NewChoiceFromState(dbStep, node.(*grammar.ChoiceState), svc)
+	// case grammar.StateType.Map:
+	// 	step, err = NewMapFromState(dbStep, node, svc)
+	// case grammar.StateType.Pass:
+	// 	step, err = NewPassFromState(dbStep, node.(*grammar.PassState), svc)
+	// case grammar.StateType.Parallel:
+	// 	step, err = NewParallelFromState(dbStep, node.(*grammar.ParallelState), svc)
+	// case grammar.StateType.StateGroup:
+	// 	step, err = NewStepGroupFromState(dbStep, node.(*grammar.StateGroup), svc)
+	// case grammar.StateType.Suspend:
+	// 	step, err = NewSuspendFromState(dbStep, node.(*grammar.SuspendState), svc)
+	// case grammar.StateType.Wait:
+	// 	step, err = NewWaitFromState(dbStep, node.(*grammar.WaitState), svc)
+	// case grammer.StateType.Fail:
+	// 	state, err = NewFailFromData(dbstep)
+	// case grammer.StateType.Succeed:
+	// 	state, err = NewSucceedFromData(dbstep)
+	default:
+		err = fmt.Errorf("state [ %s ] type : [ %s ] not supported", dbStep.Name, dbStep.Type)
 
-// 	switch dbStep.Type {
-// 	case grammar.StateType.Task:
-// 		step, err = NewTaskFromState(dbStep, node.(*grammar.TaskState), svc)
-// 	case grammar.StateType.Choice:
-// 		step, err = NewChoiceFromState(dbStep, node.(*grammar.ChoiceState), svc)
-// 	case grammar.StateType.Map:
-// 		step, err = NewMapFromState(dbStep, node, svc)
-// 	case grammar.StateType.Pass:
-// 		step, err = NewPassFromState(dbStep, node.(*grammar.PassState), svc)
-// 	case grammar.StateType.Parallel:
-// 		step, err = NewParallelFromState(dbStep, node.(*grammar.ParallelState), svc)
-// 	case grammar.StateType.StateGroup:
-// 		step, err = NewStepGroupFromState(dbStep, node.(*grammar.StateGroup), svc)
-// 	case grammar.StateType.Suspend:
-// 		step, err = NewSuspendFromState(dbStep, node.(*grammar.SuspendState), svc)
-// 	case grammar.StateType.Wait:
-// 		step, err = NewWaitFromState(dbStep, node.(*grammar.WaitState), svc)
-// 	// case grammer.StateType.Fail:
-// 	// 	state, err = NewFailFromData(dbstep)
-// 	// case grammer.StateType.Succeed:
-// 	// 	state, err = NewSucceedFromData(dbstep)
-// 	default:
-// 		err = fmt.Errorf("state [ %s ] type : [ %s ] unrecognize ", dbStep.Name, dbStep.Type)
-
-// 	}
-// 	return step, err
-// }
+	}
+	return step, err
+}
