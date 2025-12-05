0d9c17d4c121c68e1fb8e6261102e66b1eb7fd49
mmtbak <47457296+mmtbak@users.noreply.github.com>
2025-12-05 18:05:42 +0800

    stash

--- workflow/executor/basestep.go
+++ workflow/executor/basestep.go
@@ -151,7 +151,7 @@ func (step *ExecutionStep) Init(msg queue.InnerMessageBody) error {
 		}
 	} else {
 		// 减少一次额外的查询
-		dbExecution, err = step.ExecutionService.QueryExecutionByID(dbStep.ExecutionID, []string{"id", "max_execute_index"}, tx)
+		dbExecution, err = step.ExecutionService.QueryExecutionByID(step.Data.ExecutionID, []string{"id", "max_execute_index"}, tx)
 		if err != nil {
 			return err
 		}
